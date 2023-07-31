197c694bf98efbff0cb4fcb62d54f946680d7ebf
Krystle Lee <l33.krys@gmail.com>
2023-07-31 10:34:52 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  if (x <= 400) {
+    return `This one is on me!`;
+  } else if (x > 400 && x <= 2000) {
+    return `That will be twenty bucks.`;
+  } else if (x > 2000 && x <2500) {
+    return `I will gladly take your thirty bucks.`;
+  } else {
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === `NYC` ? `Ok, sounds good.` : `No go.`; 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case `generous`:
+    return `Thank you so much.`;
+    break;
+  case `not as generous`:
+    return `Thank you.`;
+    break;
+  default:
+    return `Bye.`;
+    break;
+  }
 }
